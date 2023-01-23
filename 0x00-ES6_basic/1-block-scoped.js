bed9d8899cfa30e46ce2ee3914bb818990196577
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2023-01-24 00:04:58 +0100

    Non overwritten, changedd not to hoiste in block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,6 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /* type changed to avoid hoisting */
     const task = true;
     const task2 = false;
   }
