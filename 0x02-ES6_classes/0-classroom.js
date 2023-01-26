ef9f501848673cb8680a0fa5ed757a1a704a7ba1
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2023-01-26 10:56:30 +0100

    task pone solved for javascript classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+/*
+  This is the definition of a classRoom,
+  with a default initializer maxStudentsSize
+*/
+class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
+
+export default ClassRoom
