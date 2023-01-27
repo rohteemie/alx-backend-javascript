71b531a4b7ee4446e8f21b28e6684afabf2a6c63
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2023-01-27 04:50:03 +0100

    eslinting error fixed

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,7 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
-    }
-    else
-    reject(new Error('The fake API is not working currently'));
+    } else reject(new Error('The fake API is not working currently'));
   });
-};
-
-export default getFullResponseFromAPI;
+}
