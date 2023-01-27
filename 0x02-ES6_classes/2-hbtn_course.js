974a296bfb64cbc0fb487116bb4c5eec11eb6cc3
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2023-01-27 05:50:46 +0100

    code fixed to catch errors.

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,15 +1,39 @@
-
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof (name) === 'string') { this._name = name; } else { throw new TypeError(); }
+
+    if (typeof (length) === 'number') { this._length = length; } else { throw new TypeError(); }
+
+    if (Array.isArray(students)) { this._students = students; } else { throw new TypeError(); }
   }
+
   get name() { return this._name; }
+
   get length() { return this._length; }
+
   get students() { return this._students; }
 
-  set name(name) { this._name = name; }
-  set length(length) { this._length = length; }
-  set students(students) { this._students = students; }
+  set name(name) {
+    if (typeof (name) === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  set length(length) {
+    if (typeof (length) === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  set students(students) {
+    if (Array.isArray(students)) {
+      this._students = students;
+    } else {
+      throw new TypeError('students must be an array');
+    }
+  }
 }
