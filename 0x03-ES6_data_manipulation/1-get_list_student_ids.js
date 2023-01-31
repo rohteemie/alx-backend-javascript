394d919863c8acb43cdb39d92e1908e15eff1aaf
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2023-01-31 07:05:47 +0100

    1. More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(arrObj) {
+  if (Array.isArray(arrObj)) {
+    return arrObj.map((obj) => obj.id);
+  } else {
+    return '[]';
+  }
+}
+
---
+++ 0x03-ES6_data_manipulation/test/objTest.js
@@ -0,0 +1,7 @@
+const arr = [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' }
+];
+
+for ( name of arr ) {console.log(name.id)};
