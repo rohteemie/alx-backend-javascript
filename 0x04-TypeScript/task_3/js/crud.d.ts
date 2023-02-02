f17b1b6305e37050d6ef2696f6a30b4c19e4f9c4
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2023-02-02 05:44:11 +0100

    Deleted all misc files

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++
@@ -1,5 +0,0 @@
-import {RowElement, RowID} from "./interface";
-
-declare function insertRow(row: RowElement): number;
-declare function deleteRow(rowId: RowID): number;
-declare function updateRow(rowId: RowID, row: RowElement): number;
